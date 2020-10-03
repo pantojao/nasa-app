cbfa49715392d5d13d0a5dc49c50eceec64a63e6
pantojao <61061684+pantojao@users.noreply.github.com>
2020-10-02 20:02:19 -0700

    Updates

---
+++ precache-manifest.4a5f768bc337d9442ecf0da796a091bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c74f5c944e700c2a1457b815ce361e24",
+    "url": "/nasa-app/index.html"
+  },
+  {
+    "revision": "31dd522eee09eb628469",
+    "url": "/nasa-app/static/css/main.830997e6.chunk.css"
+  },
+  {
+    "revision": "0a7887a69a93276463e4",
+    "url": "/nasa-app/static/js/2.f7acb236.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nasa-app/static/js/2.f7acb236.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31dd522eee09eb628469",
+    "url": "/nasa-app/static/js/main.78413953.chunk.js"
+  },
+  {
+    "revision": "c3970e2f9a98ce78f24e",
+    "url": "/nasa-app/static/js/runtime-main.1c4d3ad0.js"
+  }
+]);
